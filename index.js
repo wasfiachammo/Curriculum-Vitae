d1253ed63947e2af868cb5754327ec4cea32f1c9
wasfiachammo <wasfiachammo@gmail.com>
2022-10-02 14:33:20 +0300

    First

---
+++ index.js
@@ -0,0 +1,49 @@
+var services=document.getElementById('Services')
+var original_content=`            
+
+<div class="icon"><i class="bi bi-laptop"></i></div>
+<h4 class="title"><a href="">Title</a></h4>
+<p class="description">
+ Description
+</p>
+</div>`
+var intersteds=document.getElementById('Intersted')
+var org_content=`
+<div class="icon"><i class="bi bi-clipboard-heart"></i></div>
+<h4 class="title"><a href="">Hobbies</a></h4>
+<p class="description">
+ Descriptions
+</p>
+</div>
+`
+fetch('http://localhost:3000/services')
+  .then(response => response.json())
+  .then(json => {
+    json.Sheet1.forEach(element => {
+        var content=original_content;
+        content=content.replace('Title',element.A)
+        content=content.replace('Description',element.B)
+        
+       
+        var service=document.createElement('div');
+        service.innerHTML=content;
+        service.className="col-lg-4 col-md-6 icon-box";
+        services.appendChild(service);
+    });
+    json.Sheet2.forEach(element=>{
+        var content=org_content;
+        content=content.replace('Hobbies',element.A)
+        content=content.replace('Descriptions',element.B)
+        var hob=document.createElement('div');
+        hob.innerHTML=content;
+        hob.className="col-lg-4 col-md-6 icon-box";
+        intersteds.appendChild(hob);
+
+
+    })
+
+
+
+  }
+  
+  )
